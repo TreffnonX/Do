867e09accce4b5b6f3cb63ec368a3ca286691019
TreffnonX <janis@schoeck-netz.de>
2017-08-01 20:03:01 +0200

    Initial Commit
    Upload the basic utility and .gitignore

---
+++ Do/src/eu/graphitez/utils/ldo/Do.java
@@ -0,0 +1,105 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 TreffnonX (Janis Schöck)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package eu.graphitez.utils.ldo;
+
+/**
+ * Do is a lambda utility to help minimize the overhead for writing lambda expressions.
+ * <br>
+ * Lambda expressions help a lot in minimizing formerly bulky and hard to handle code.
+ * However the simplicity of the syntax strongly depends on whether or not a lambda can be
+ * expressed as a single meaningful expression, rather than a short series of expressions.
+ * <br>
+ * This utility class provides simple methods to "cast" objects to void or to press
+ * an if-statement into a single expression to help keep lambda expressions in one line
+ * or more specifically: to avoid having to break a lambda into a statement block.
+ * 
+ * @author Janis Schöck - janis@schoeck-netz.de
+ */
+public class Do {
+	/**
+	 * <i>Sort of casts a boolean to void.</i>
+	 * 
+	 * Takes a boolean as parameter without doing anything to or with it.
+	 * This method is simply there to "destroy" a return value, in order to satisfy a
+	 * lambda expression returning void.
+	 * 
+	 * @param ignored Ignored value.
+	 */
+	public static void asVoid(boolean ignored) {
+		// This method needs to do nothing but swallow the passed object.
+	}
+	
+	/**
+	 * <i>Sort of casts an {@link Object} to void.</i>
+	 * 
+	 * Takes a generic {@link Object} as parameter without doing anything to or with it.
+	 * This method is simply there to "destroy" a return value, in order to satisfy a
+	 * lambda expression returning void.
+	 * 
+	 * @param ignored Ignored value.
+	 */
+	public static <T> void asVoid(T ignored) {
+		// This method needs to do nothing but swallow the passed object.
+	}
+	
+	/**
+	 * <i>Sort of casts an {@link Object} to primitive boolean true.</i>
+	 * 
+	 * Takes a generic {@link Object} as parameter without doing anything to or with it.
+	 * 
+	 * @param ignored Ignored value.
+	 */
+	public static <T> boolean asTrue(T ignored) {
+		return true;
+	}
+	
+	/**
+	 * Method to press an if-statement into a single expression.
+	 * Conditionally executes either of two lambdas, based on the condition parameter.
+	 * 
+	 * @param condition Condition defining which lambda is executed.
+	 * @param then Executed if the condition is true.
+	 * @param elze Executed if the condition is false.
+	 */
+	public static <T> void on(boolean condition, Runnable then, Runnable elze) {
+		if (condition) {
+			then.run();
+		} else {
+			elze.run();
+		}
+	}
+	
+	/**
+	 * Method to press an if-statement into a single expression.
+	 * Conditionally executes a lambdas, based on the condition parameter.
+	 * 
+	 * @param condition Condition defining if the lambda is executed.
+	 * @param then Executed if the condition is true.
+	 */
+	public static <T> void on(boolean condition, Runnable then) {
+		if (condition) {
+			then.run();
+		}
+	}
+}
---
+++ Do/test/eu/graphitey/utils/ldo/DoTest.java
@@ -0,0 +1,67 @@
+package eu.graphitey.utils.ldo;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import eu.graphitez.utils.ldo.Do;
+
+/**
+ * @author Janis Schöck - janis@schoeck-netz.de
+ */
+public class DoTest {
+	@SuppressWarnings("unused")
+	@Test
+	public void test_asVoid() {
+		// This method actually is a compiler test and needs no execution to prove validity.
+		
+		// An expression can return some value object, even though the lambda expression should infer void.
+		// hence this call serves as "cast to void".
+		Do.asVoid(true ? /* any expression returning anything */null : /* another expression */null);
+		
+		// The statement behind the "&&" is only executed after the left side has been checked. This allows for
+		// a more convenient if-expression instead of an if-statement.
+		Do.asVoid(true && Do.asTrue(/* any expression returning anything */null));
+	}
+	
+	@Test
+	public void test_onTrue() {
+		VBoolean executed = new VBoolean();
+		// Is the runnable executed?
+		Do.on(true, () -> executed.m_value = true);
+		assertTrue(executed.m_value);
+	}
+	
+	@Test
+	public void test_onFalse() {
+		VBoolean executed = new VBoolean();
+		// Is the runnable executed?
+		Do.on(false, () -> executed.m_value = true);
+		assertFalse(executed.m_value);
+	}
+	
+	@Test
+	public void test_onThen() {
+		VBoolean then = new VBoolean();
+		VBoolean elze = new VBoolean();
+		// Is the correct runnable executed?
+		Do.on(true, () -> then.m_value = true, () -> elze.m_value = true);
+		assertTrue(then.m_value);
+		assertFalse(elze.m_value);
+	}
+	
+	@Test
+	public void test_onElse() {
+		VBoolean then = new VBoolean();
+		VBoolean elze = new VBoolean();
+		// Is the correct runnable executed?
+		Do.on(false, () -> then.m_value = true, () -> elze.m_value = true);
+		assertFalse(then.m_value);
+		assertTrue(elze.m_value);
+	}
+	
+	public static class VBoolean {
+		public boolean m_value = false;
+	}
+}
